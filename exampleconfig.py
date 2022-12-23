f77539062a4ceb921135804b85d0cd02dd5ed9b6
vtvit <111300830+vtvit@users.noreply.github.com>
2022-12-23 22:29:38 +0300

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -2,23 +2,14 @@ from sample_config import Config
 
 
 class Development(Config):
-    # get this values from the my.telegram.org
-    APP_ID = 6
-    API_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e"
-    # the name to display in your alive message
-    ALIVE_NAME = "Your value"
-    # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
-    DB_URI = "Your value"
-    # After cloning the repo and installing requirements do python3 telesetup.py an fill that value with this
-    STRING_SESSION = "Your value"
-    # create a new bot in @botfather and fill the following vales with bottoken and username respectively
-    TG_BOT_TOKEN = "Your value"
-    TG_BOT_USERNAME = "Your value"
-    # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
-    PRIVATE_GROUP_BOT_API_ID = -100
-    # command handler
-    COMMAND_HAND_LER = "."
-    # sudo enter the id of sudo users userid's in that array
-    SUDO_USERS = []
-    # command hanler for sudo
-    SUDO_COMMAND_HAND_LER = "."
+    # لەجێی ئەو وشانەی بە کوردی نوسراوە داواکاریەکان دابنێ
+    APP_ID = 6 #"ئایپی ئایدی"   
+    API_HASH = "ئایپی هاش"
+    ALIVE_NAME = "ناوی ئەکاونتت "
+    DB_URI = "بەستەری سەیڤکراوی تایبەت بەتۆ"
+    STRING_SESSION = "كۆد تێرمۆکس"
+    TG_BOT_TOKEN = "تۆکنی بۆتی تایبەت بە تۆ "
+    PRIVATE_GROUP_BOT_API_ID = -100  # زیادی بکە لەگەڵ ناسنامەی(ئایدی) گرووپا -100 مەیسڕەوە
+    COMMAND_HAND_LER = "."  # لێی گەڕێ
+    SUDO_COMMAND_HAND_LER = "."  # لێی گەڕێ
+    TZ = "Asia/Baghdad"  #لێی گەڕێ
