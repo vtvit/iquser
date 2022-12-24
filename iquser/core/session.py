c0c4d412229fc7aecd590a0fd54fdfe170e7d3a5
vtvit <111300830+vtvit@users.noreply.github.com>
2022-12-24 13:34:08 +0300

    Update session.py

--- iquser/core/session.py
+++ iquser/core/session.py
@@ -1,23 +1,24 @@
 import sys
-from iquser.core.logger import logging
-from telethon.network.connection.tcpabridged import ConnectionTcpAbridged
-from telethon.sessions import StringSession
-from telethon.errors import AccessTokenExpiredError, AccessTokenInvalidError
-from ..Config import Config
-from .client import vtvitClient
-LOGS = logging.getLogger(" ")
 
+from telethon.network.connection.tcpabridged import ConnectionTcpAbridged
+
+from ..Config import Config
+from .bothseesion import bothseesion
+from .client import IQUserBotClient
+from .logger import logging
+
+LOGS = logging.getLogger("𝙄𝙌𝙐𝙎𝙀𝙍 メ")
 __version__ = "2.10.6"
 
 loop = None
 
 if Config.STRING_SESSION:
-    session = StringSession(str(Config.STRING_SESSION))
+    session = bothseesion(Config.STRING_SESSION, LOGS)
 else:
     session = "iquser"
 
 try:
-    iquser = vtvitClient(
+    iqub = IQUserBotClient(
         session=session,
         api_id=Config.APP_ID,
         api_hash=Config.API_HASH,
@@ -28,21 +29,18 @@ try:
         connection_retries=None,
     )
 except Exception as e:
-    print(f"[STRING SESSION] - {str(e)}")
+    print(
+        f"STRING_SESSION CODE WRONG MAKE A NEW SESSION - {e}\n کۆدی سیشنی تێلثۆن نادروستە  ..  تکایە کۆدی نوێ دەربکە ؟!"
+    )
     sys.exit()
 
-try:
-    jepiq.tgbot = tgbot = vtvitClient(
-        session="arTgbot",
-        api_id=Config.APP_ID,
-        api_hash=Config.API_HASH,
-        loop=loop,
-        app_version=__version__,
-        connection=ConnectionTcpAbridged,
-        auto_reconnect=True,
-        connection_retries=None,
-    ).start(bot_token=Config.TG_BOT_TOKEN)
-except AccessTokenExpiredError:
-    LOGS.error("تۆکنی بۆت بەسەرچووە جێگۆڕکێی دەکات بۆ ئەوەی سەرچاوەکە کار بکات")
-except AccessTokenInvalidError:
-    LOGS.error("تۆکنی بۆت شوێنی ناردوستە بۆ سەرچاوەکە بۆ ئەوەی کاربکات")
+iqub.tgbot = tgbot = IQUserBotClient(
+    session="IQTgbot",
+    api_id=Config.APP_ID,
+    api_hash=Config.API_HASH,
+    loop=loop,
+    app_version=__version__,
+    connection=ConnectionTcpAbridged,
+    auto_reconnect=True,
+    connection_retries=None,
+).start(bot_token=Config.TG_BOT_TOKEN)
