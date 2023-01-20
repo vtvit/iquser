71aa1ee1b973828a20c0e65cf3892ab2cadcc9fc
حـەمـە <111300830+vtvit@users.noreply.github.com>
2023-01-20 15:33:45 +0300

    Update gban_sql_helper.py

--- iquser/sql_helper/gban_sql_helper.py
+++ iquser/sql_helper/gban_sql_helper.py
@@ -1,5 +1,5 @@
 """
-credits to @mrconfused and @lMl10l
+credits to @mrconfused and @VTVIT
 """
 #    Copyright (C) 2020  sandeep.n(π.$)
 #    This program is free software: you can redistribute it and/or modify
