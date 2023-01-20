a3084d78e6dba33e25801ac4cda61cfa4ef2e702
حـەمـە <111300830+vtvit@users.noreply.github.com>
2023-01-20 23:29:23 +0300

    Update memeifyhelpers.py

--- iquser/helpers/memeifyhelpers.py
+++ iquser/helpers/memeifyhelpers.py
@@ -129,6 +129,6 @@ async def silently_send_message(conv, text):
 
 
 async def thumb_from_audio(audio_path, output):
-    await _catutils.runcmd(
+    await _iqutils.runcmd(
         f"ffmpeg -i {audio_path} -filter:v scale=500:500 -an {output}"
     )
