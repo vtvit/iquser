ee9d5331cb43785cb446f883b624603c76812918
حـەمـە <111300830+vtvit@users.noreply.github.com>
2023-01-22 12:43:37 +0300

    Update session.py

--- iquser/core/session.py
+++ iquser/core/session.py
@@ -4,17 +4,15 @@ from telethon.network.connection.tcpabridged import ConnectionTcpAbridged
 from telethon.sessions import StringSession
 from telethon.errors import AccessTokenExpiredError, AccessTokenInvalidError
 from ..Config import Config
-from .bothseesion import bothseesion
 from .client import IQUserBotClient
-from .logger import logging
+LOGS = logging.getLogger(" ")
 
-LOGS = logging.getLogger("𝙄𝙌𝙐𝙎𝙀𝙍 メ")
 __version__ = "2.10.6"
 
 loop = None
 
 if Config.STRING_SESSION:
-    session = bothseesion(Config.STRING_SESSION, LOGS)
+    session = StringSession(str(Config.STRING_SESSION))
 else:
     session = "iquser"
 
@@ -30,18 +28,21 @@ try:
         connection_retries=None,
     )
 except Exception as e:
-    print(
-        f"STRING_SESSION CODE WRONG MAKE A NEW SESSION - {e}\n کۆدی سیشنی تێلثۆن نادروستە  ..  تکایە کۆدی نوێ دەربکە ؟!"
-    )
+    print(f"[STRING SESSION] - {str(e)}")
     sys.exit()
 
-iqub.tgbot = tgbot = IQUserBotClient(
-    session="IQTgbot",
-    api_id=Config.APP_ID,
-    api_hash=Config.API_HASH,
-    loop=loop,
-    app_version=__version__,
-    connection=ConnectionTcpAbridged,
-    auto_reconnect=True,
-    connection_retries=None,
-).start(bot_token=Config.TG_BOT_TOKEN)
+try:
+    iqub.tgbot = tgbot = IQUserBotClient(
+        session="iqTgbot",
+        api_id=Config.APP_ID,
+        api_hash=Config.API_HASH,
+        loop=loop,
+        app_version=__version__,
+        connection=ConnectionTcpAbridged,
+        auto_reconnect=True,
+        connection_retries=None,
+    ).start(bot_token=Config.TG_BOT_TOKEN)
+except AccessTokenExpiredError:
+    LOGS.error("ئەگەر تۆکنی بۆت بەسەرچووە، بیگۆڕە بۆ ئەوەی سەرچاوە کار بکات")
+except AccessTokenInvalidError:
+    LOGS.error("تۆکنی بۆتەکە نادروستە، بیگۆڕە بۆ ئەوەی سەرچاوەکە کاربکا")
