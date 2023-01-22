b2a8058686d0e4cd1d14024b3990d446fc7204cf
حـەمـە <111300830+vtvit@users.noreply.github.com>
2023-01-22 10:54:42 +0300

    Update stringsetup.py

--- stringsetup.py
+++ stringsetup.py
@@ -1,4 +1,4 @@
-!/usr/bin/env python3
+#!/usr/bin/env python3
 # (c) https://t.me/TelethonChat/37677
 # This Source Code Form is subject to the terms of the GNU
 # General Public License, v.3.0. If a copy of the GPL was not distributed with this
@@ -7,14 +7,9 @@
 from telethon.sessions import StringSession
 from telethon.sync import TelegramClient
 
-print(
-    """Please go-to my.telegram.org
-Login using your Telegram account
-Click on API Development Tools
-Create a new application, by entering the required details"""
-)
-APP_ID = int(input("Enter APP ID here: "))
-API_HASH = input("Enter API HASH here: ")
+print("- Code telethon For ch : @IQUSER0")
+APP_ID = int(input("APP ID here: "))
+API_HASH = input("API HASH here: ")
 
 with TelegramClient(StringSession(), APP_ID, API_HASH) as client:
     print(client.session.save())
