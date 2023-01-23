ba0e8a4177ac955f4dc482ca3f867fbe340929f1
حـەمـە <111300830+vtvit@users.noreply.github.com>
2023-01-23 14:28:37 +0300

    Update __init__.py

--- iquser/assistant/__init__.py
+++ iquser/assistant/__init__.py
@@ -1,4 +1,4 @@
 from iquser import BOTLOG, BOTLOG_CHATID, iqub
-from IQUSER0.razan.resources.assistant import *
+from iqub.razan.resources.assistant import *
 from ..Config import Config
 from ..core.inlinebot import *
