db102f4da4e3d8db92e573c0b7309f19f7910031
حـەمـە <111300830+vtvit@users.noreply.github.com>
2023-01-23 10:36:18 +0300

    Update __init__.py

--- iquser/helpers/utils/__init__.py
+++ iquser/helpers/utils/__init__.py
@@ -6,8 +6,8 @@ check = 0
 while flag:
     try:
         from . import format as _format
-        from . import tools as _iqusertools
-        from . import utils as _iquserutils
+        from . import tools as _cattools
+        from . import utils as _catutils
         from .events import *
         from .format import *
 
